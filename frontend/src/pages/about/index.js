7d7628c0e5f016965d540af201a592f55075c155
Maksim <124262417+Screameerrr@users.noreply.github.com>
2024-11-11 11:08:21 +0500

    Update index.js

--- frontend/src/pages/about/index.js
+++ frontend/src/pages/about/index.js
@@ -3,53 +3,50 @@ import styles from './styles.module.css'
 import MetaTags from 'react-meta-tags'
 
 const About = ({ updateOrders, orders }) => {
-  
-  return <Main>
-    <MetaTags>
-      <title>О проекте</title>
-      <meta name="description" content="Фудграм - О проекте" />
-      <meta property="og:title" content="О проекте" />
-    </MetaTags>
-    
-    <Container>
-      <h1 className={styles.title}>Привет!</h1>
-      <div className={styles.content}>
-        <div>
-          <h2 className={styles.subtitle}>Что это за сайт?</h2>
-          <div className={styles.text}>
-            <p className={styles.textItem}>
-              Представляю вам проект, созданный во время обучения в Яндекс Практикуме. Этот проект — часть учебного курса, но он создан полностью самостоятельно.
-            </p>
-            <p className={styles.textItem}>
-              Цель этого сайта — дать возможность пользователям создавать и хранить рецепты на онлайн-платформе. Кроме того, можно скачать список продуктов, необходимых для
-              приготовления блюда, просмотреть рецепты друзей и добавить любимые рецепты в список избранных.
-            </p>
-            <p className={styles.textItem}>
-              Чтобы использовать все возможности сайта — нужна регистрация. Проверка адреса электронной почты не осуществляется, вы можете ввести любой email. 
-            </p>
-            <p className={styles.textItem}>
-              Заходите и делитесь своими любимыми рецептами!
-            </p>
+  return (
+    <Main>
+      <MetaTags>
+        <title>О проекте</title>
+        <meta name="description" content="Фудграм - О проекте" />
+        <meta property="og:title" content="О проекте" />
+      </MetaTags>
+
+      <Container>
+        <h1 className={styles.title}>Привет!</h1>
+        <div className={styles.content}>
+          <div>
+            <h2 className={styles.subtitle}>Что это за сайт?</h2>
+            <div className={styles.text}>
+              <p className={styles.textItem}>
+                Представляю вам проект, созданный во время обучения в Яндекс Практикуме. Этот проект — часть учебного курса, но он создан полностью самостоятельно.
+              </p>
+              <p className={styles.textItem}>
+                Цель этого сайта — дать возможность пользователям создавать и хранить рецепты на онлайн-платформе. Кроме того, можно скачать список продуктов, необходимых для
+                приготовления блюда, просмотреть рецепты друзей и добавить любимые рецепты в список избранных.
+              </p>
+              <p className={styles.textItem}>
+                Чтобы использовать все возможности сайта — нужна регистрация. Проверка адреса электронной почты не осуществляется, вы можете ввести любой email.
+              </p>
+              <p className={styles.textItem}>
+                Заходите и делитесь своими любимыми рецептами!
+              </p>
+            </div>
           </div>
+          <aside>
+            <h2 className={styles.additionalTitle}>Ссылки</h2>
+            <div className={styles.text}>
+              <p className={styles.textItem}>
+                Код проекта находится тут - <a href="https://github.com/Screameerrr/foodgram" className={styles.textLink}>Github</a>
+              </p>
+              <p className={styles.textItem}>
+                Автор проекта: <a href="https://yandex.ru/images/touch/search?text=Мем&img_url=https%3A%2F%2Fcs.pikabu.ru%2Fpost_img%2Fbig%2F2013%2F03%2F06%2F7%2F1362561290_236815809.jpg&pos=31&rpt=simage" className={styles.textLink} target="_blank" rel="noopener noreferrer">Шевчук Максим</a>
+              </p>
+            </div>
+          </aside>
         </div>
-        <aside>
-          <h2 className={styles.additionalTitle}>
-            Ссылки
-          </h2>
-          <div className={styles.text}>
-            <p className={styles.textItem}>
-              Код проекта находится тут - <a href="https://github.com/Screameerrr/foodgram" className={styles.textLink}>Github</a>
-            </p>
-            <p className={styles.textItem}>
-              Автор проекта: <a href="https://yandex.ru/images/touch/search?text=Мем&img_url=https%3A%2F%2Fcs.pikabu.ru%2Fpost_img%2Fbig%2F2013%2F03%2F06%2F7%2F1362561290_236815809.jpg&pos=31&rpt=simage" className={styles.textLink}>Шевчук Максим</a>
-            </p>
-          </div>
-        </aside>
-      </div>
-      
-    </Container>
-  </Main>
+      </Container>
+    </Main>
+  )
 }
 
 export default About
-
