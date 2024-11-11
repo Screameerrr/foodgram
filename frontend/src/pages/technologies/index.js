7f3e739d8b9de71857b53a87cf1acaae50480845
Maksim <124262417+Screameerrr@users.noreply.github.com>
2024-11-11 11:06:00 +0500

    Update index.js

--- frontend/src/pages/technologies/index.js
+++ frontend/src/pages/technologies/index.js
@@ -3,39 +3,48 @@ import styles from './styles.module.css'
 import MetaTags from 'react-meta-tags'
 
 const Technologies = () => {
-  
-  return <Main>
-    <MetaTags>
-      <title>О проекте</title>
-      <meta name="description" content="Фудграм - Технологии" />
-      <meta property="og:title" content="О проекте" />
-    </MetaTags>
-    
-    <Container>
-      <h1 className={styles.title}>Технологии</h1>
-      <div className={styles.content}>
-        <div>
-          <h2 className={styles.subtitle}>Технологии, которые применены в этом проекте:</h2>
-          <div className={styles.text}>
-            <ul className={styles.textItem}>
-              <li className={styles.textItem}>
-                Python
-              </li>
-              <li className={styles.textItem}>
-                Django
-              </li>
-              <li className={styles.textItem}>
-                Django REST Framework
-              </li>
-              <li className={styles.textItem}>
-                Djoser
-              </li>
-            </ul>
+  return (
+    <Main>
+      <MetaTags>
+        <title>О проекте</title>
+        <meta name="description" content="Фудграм - Технологии" />
+        <meta property="og:title" content="О проекте" />
+      </MetaTags>
+
+      <Container>
+        <h1 className={styles.title}>Технологии</h1>
+        <div className={styles.content}>
+          <div>
+            <h2 className={styles.subtitle}>Технологии, которые применены в этом проекте:</h2>
+            <div className={styles.text}>
+              <ul className={styles.textItem}>
+                <li className={styles.textItem}>
+                  <a href="https://www.python.org/" target="_blank" rel="noopener noreferrer">
+                    Python
+                  </a>
+                </li>
+                <li className={styles.textItem}>
+                  <a href="https://www.djangoproject.com/" target="_blank" rel="noopener noreferrer">
+                    Django
+                  </a>
+                </li>
+                <li className={styles.textItem}>
+                  <a href="https://www.django-rest-framework.org/" target="_blank" rel="noopener noreferrer">
+                    Django REST Framework
+                  </a>
+                </li>
+                <li className={styles.textItem}>
+                  <a href="https://djoser.readthedocs.io/" target="_blank" rel="noopener noreferrer">
+                    Djoser
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-    </Container>
-  </Main>
+      </Container>
+    </Main>
+  )
 }
 
 export default Technologies
